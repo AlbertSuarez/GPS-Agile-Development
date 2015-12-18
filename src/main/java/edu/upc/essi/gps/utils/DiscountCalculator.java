5c2dda9b3c5bbb4cd211bcfa57dfe2ee34d5875f
David Morán <davidmoranpomes@gmail.com>
2015-12-18 01:45:57 +0100

    permutations applied

--- src/main/java/edu/upc/essi/gps/utils/DiscountCalculator.java
+++ src/main/java/edu/upc/essi/gps/utils/DiscountCalculator.java
@@ -6,10 +6,7 @@ import edu.upc.essi.gps.domain.discounts.Discount;
 import edu.upc.essi.gps.domain.lines.SaleLine;
 import edu.upc.essi.gps.ecommerce.services.DiscountService;
 
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -63,7 +60,7 @@ public final class DiscountCalculator {
                 .sorted(((o1, o2) -> o1.getName().compareTo(o2.getName())))
                 .collect(Collectors.toList());
 
-        return computateBestDiscountCombination(descomptesDisponibles, productesDisponibles);
+        return computeBestDiscountCombination(descomptesDisponibles, productesDisponibles);
     }
 
     private static boolean isAplicable(Discount discount, Sale currentSale) {
@@ -83,22 +80,68 @@ public final class DiscountCalculator {
         return someLineIsTrigger && hasAllNecessaryProducts;
     }
 
-    private static Double computateBestDiscountCombination(List<Discount> discounts, List<Product> products) {
+    private static Double computeBestDiscountCombination(List<Discount> discounts, List<Product> products) {
         //TENIM: discounts(List<Discount>), products(List<Product>)
         //PRE: No hi ha elements repetits a "discounts"
 
         //CAL: generar totes les permutacions en l'ordre d'aplicació dels descomptes i substracció de productes utilitzats.
 
-        return computateBestDiscountRecursive(discounts, products, 0, 0d);
+        List<List<Discount>> permutations = getPermutations(discounts);
+
+        Double max = 0d;
+        Double val;
+
+        System.out.println("Permutations");
+        for (List<Discount> list : permutations) {
+            System.out.println(list);
+            val = computeBestDiscountRecursive(list, products, 0, 0d);
+            System.out.println(val);
+            if (val > max) max = val;
+        }
+
+        return max;
     }
 
-    private static Double computateBestDiscountRecursive(List<Discount> discounts, List<Product> products, int i, Double acumulated) {
+    /**
+     * Donada una permutació de descomptes, en calcula el màxim valor.
+     * */
+    private static Double computeBestDiscountRecursive(List<Discount> discounts, List<Product> products, int i, Double acumulated) {
         if (i >= discounts.size()) return acumulated;
-        Double A = computateBestDiscountRecursive(discounts, new LinkedList<>(products), i+1, acumulated);
-        DiscountHolder holder = discounts.get(i).calculate(products);
-        products.removeAll(holder.getRequired());
-        Double B = computateBestDiscountRecursive(discounts, new LinkedList<>(products), i+1, acumulated + holder.getPrice());
+        Double A = computeBestDiscountRecursive(discounts, new LinkedList<>(products), i + 1, acumulated);
+        DiscountHolder holder = discounts.get(i).calculate(new LinkedList<>(products));
+        List<Product> listB = new LinkedList<>(products);
+        listB.removeAll(holder.getRequired());
+        Double B = computeBestDiscountRecursive(discounts, listB, i + 1, acumulated + holder.getPrice());
         return Math.max(A, B);
     }
+
+
+    public static List<List<Discount>> getPermutations(List<Discount> discounts){
+        if(discounts.size() == 1) {
+            List<Discount> list = new LinkedList<>();
+            list.add(discounts.get(0));
+            List<List<Discount>> listOfList = new LinkedList<>();
+            listOfList.add(list);
+            return listOfList;
+        }
+
+        Set<Discount> set = new HashSet<>(discounts);
+
+        List<List<Discount>> listOfLists = new LinkedList<>();
+
+        for(Discount d: discounts){
+            Set<Discount> setOfCopied = new HashSet<>(set);
+            setOfCopied.remove(d);
+
+            List<List<Discount>> permute = getPermutations(new ArrayList<>(setOfCopied));
+            for (List<Discount> perm : permute) {
+                perm.add(d);
+                listOfLists.add(perm);
+            }
+        }
+
+        return listOfLists;
+    }
+
 
 }
